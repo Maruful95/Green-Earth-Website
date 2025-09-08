1b6d3e69a9e37ef1ee512601e8105f8c56892e09
Maruful95 <maruful94955@gmail.com>
2025-09-08 23:47:16 +0600

    added modal

--- index.js
+++ index.js
@@ -11,6 +11,29 @@ const removeActive = () => {
   categoryButton.forEach((btn) => btn.classList.remove("active"));
 };
 
+// function for modal tree details
+const loadTreeDetails = (id) => {
+    const url = `https://openapi.programming-hero.com/api/plant/${id}`
+    fetch(url)
+        .then((res) => res.json())
+    .then((json)=> displayDetails(json.plants))
+}
+// display details 
+const displayDetails = (detail) => {
+    const detailsContainer = document.getElementById("details-container");
+    detailsContainer.innerHTML = `
+    
+    <h3 class="font-bold text-sm mt-1">${detail.name}</h3>
+    <img class="mt-2 max-h-70 w-full object-cover rounded-md" src="${detail.image}" alt="">
+        <p class="mt-2 text-sm"><span class="font-semibold">Category:</span> ${detail.category}</p>
+        <p class="mt-2 font-semibold text-sm"><span class="font-semibold">Price:</span> ৳${detail.price}</p>
+        <p class="mt-2 font-normal text-xs"><span class="font-semibold">Description:</span> ${detail.description}</p>
+    
+    `
+
+    document.getElementById("my_modal_5").showModal()
+}
+
 // tree card loading function
 const loadTreeCard = (id) => {
   const url = `https://openapi.programming-hero.com/api/category/${id}`;
@@ -35,7 +58,7 @@ const displayTreeCard = (cards) => {
         <div class="rounded-md bg-white shadow-sm">
             <img class="h-48 w-full object-cover rounded-t-md" src="${card.image}" alt="">
             <div class="px-4 pb-4 flex flex-col justify-between h-60">
-                <h3 class="font-semibold text-sm mt-3">${card.name}</h3>
+                <h3 onclick="loadTreeDetails(${card.id})" class="font-semibold text-sm mt-3">${card.name}</h3>
                 <p class="font-normal text-xs mt-2">${card.description}</p>
                 <div class="flex justify-between items-center mt-2">
                     <p class="px-2 py-1 font-medium text-sm text-[#15803D] rounded-full bg-[#DCFCE7]">${card.category}</p>
